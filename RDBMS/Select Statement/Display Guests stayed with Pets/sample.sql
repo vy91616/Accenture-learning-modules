45a37cbf9190b9ec3fdf7afee6eb4d671b4b8d58
vy91616 <53231446+vy91616@users.noreply.github.com>
2021-08-12 02:04:27 +0530

    Update sample.sql

--- RDBMS/Select Statement/Display Guests stayed with Pets/sample.sql
+++ RDBMS/Select Statement/Display Guests stayed with Pets/sample.sql
@@ -1,4 +1,4 @@
- select guestid, resortid,(todate-fromdate) as numberofdays, adultcount, childcount, petcount, totalcharge 
- from booking 
- where petcount>=1 and childcount>=0 
+ select guestid, resortid,(todate-fromdate) as NUMBER_OF_DAYS, adultcount,childcount, petcount, totalcharge
+ from booking
+ where petcount>0 
  order by petcount desc;
